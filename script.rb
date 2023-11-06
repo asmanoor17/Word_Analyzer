f135fa4377583d98523f489c01dd9311a89cbfd3
Asma Noor <asma@Asmas-MacBook-Air.local>
2023-11-06 13:25:15 -0600

    word_analyzer

--- script.rb
+++ script.rb
@@ -1,19 +1,23 @@
 words = ["madam", "nurses run", "ruby", "world"]
 
+palindrome = word.downcase == word.downcase.reverse
 for word in words 
-    palindrome = word.downcase == word.downcase.reverse
+     if palindrome 
+        puts " This #{word} is a Palindrome"
+     else 
+        puts "This #{word} is not a Palindrome"
 
     contains_e = word.downcase.include?('e')
+        if contains_e
+            puts "This #{word} contains an E"
+        else 
+            puts "This #{word} doesn't an E"
 
     # Count the total number of characters (letters) in the word or phrase without the spaces included
     character_count = word.gsub(' ', '').length
+    puts "Character Count: #{character_count}"
 
     replacement = word.gsub('ruby', 'python')
-
-    puts "Word/Phrase: #{word}"
-    puts "Palindrome: #{palindrome ? 'Yes' : 'No'}"
-    puts "Contains 'e': #{contains_e ? 'Yes' : 'No'}"
-    puts "Character Count: #{character_count}"
     puts "After Replacement: #{replacement}"
     puts "------"
 end
